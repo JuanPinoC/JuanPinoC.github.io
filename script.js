8f608e4affe5825957698c9a81029ad0502656fb
Juan Pino <juan.pino@tecsup.edu.pe>
2022-05-14 01:01:49 -0500

    Page Structure

---
+++ script.js
@@ -0,0 +1,27 @@
+let sideNavOpen = false;
+
+function initToggleNav(){
+	document.querySelector("#toggleButton").onclick = this.toggleNav;
+	document.querySelector("#toggleButton").textContent = ">" ;
+}
+
+function toggleNav() {
+
+	if( sideNavOpen ){
+		document.querySelector("aside").style.width = "0";
+		document.querySelector(".content").style.marginLeft = "0";
+		document.querySelector("#toggleButton").textContent = ">" ; //&omicron;
+	}else{
+		document.querySelector("aside").style.width = "100%";
+		document.querySelector(".content").style.marginLeft = "20rem";
+		document.querySelector("#toggleButton").textContent = "×" ; //&times;
+	}
+
+	sideNavOpen = !sideNavOpen;
+
+}
+
+window.onload = function() {
+	initToggleNav();
+};
+
